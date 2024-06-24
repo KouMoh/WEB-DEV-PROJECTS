785e6ec1f2a847657efb6b93b43cd6e9f318774d
KouMoh <126945657+KouMoh@users.noreply.github.com>
2024-06-24 14:05:39 +0530

    Add files via upload

---
+++ web-app/verify.php
@@ -0,0 +1,36 @@
+<?php
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "website");
+
+$conn = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+if ($conn->connect_error) { 
+    die("Connection failure: " . $conn->connect_error); 
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$stmt = $conn->prepare("SELECT * FROM `login` WHERE `email` = ? AND `password` = ?");
+$stmt->bind_param("ss",$email, $password);
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    
+    header("Location:http://localhost/project/pfp.html");
+    exit();
+} else {
+    
+    echo "<script>alert('Incorrect login credentials'); window.location.href = 'login.html';</script>";
+}
+
+$stmt->close();
+$conn->close();
+
+?>
